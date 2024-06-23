40252f6926889c3b5454750858adf1b1974c35e3
Zero <alengebles@gmail.com>
2024-06-23 16:58:35 +0200

    ...

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -19,7 +19,11 @@ function MainContent({ selectedCoin }){
   }, []);
 
   if (!selectedCoin) {
-    return <div className='spinnyThingy'></div>;
+    return <div className='w-screen h-screen flex items-center justify-center'>
+      <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle className="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+      </svg>
+    </div>
   }
 
   return(
